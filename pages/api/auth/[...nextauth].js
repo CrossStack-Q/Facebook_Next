c5e1de94287d1bd2662ce0022e55d0d0debf36d1
Anurag Sharma <96693300+CrossStack-Q@users.noreply.github.com>
2022-06-19 09:27:09 -0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth"
-import FacebookProvider from "next-auth/providers/facebook"
+import NextAuth from "next-auth";
+import GoogleProviders from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    GoogleProviders({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET
 })
